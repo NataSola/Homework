b424fb6e40f38dcad91c6549565f3240ca19ee6c
NataSola <nsol2009@yandex.ru>
2021-11-21 20:28:43 +0300

    Add triangle sides

---
+++ 41_TriangleSide/Program.cs
@@ -0,0 +1,13 @@
+﻿// 41. Выяснить являются ли три числа сторонами треугольника
+
+Console.Clear();
+System.Console.WriteLine();
+
+int A = 15;
+int B = 25;
+int C = 12;
+
+bool triangle = (A + B > C && A + C > B && B + C > A);
+
+if (triangle) System.Console.WriteLine("Из заданных отрезков можно составить треугольник");
+else System.Console.WriteLine("Из заданных отрезков составить треугольник невозможно");
