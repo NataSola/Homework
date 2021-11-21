321f2629e3d6a6a78f49b3cdac28a25a161f01a6
NataSola <nsol2009@yandex.ru>
2021-11-21 21:40:21 +0300

    Add count even numbers from keyboard

---
+++ 42_CountNumKeyboard/Program.cs
@@ -0,0 +1,17 @@
+﻿// 42. Определить сколько чисел больше 0 введено с клавиатуры
+
+Console.Clear();
+System.Console.WriteLine();
+int volume = 10;                // колчество ввода чисел
+int count = 0;
+
+while (volume > 0)
+{
+    System.Console.Write("Введите число:   ");
+    string num = Console.ReadLine();
+    int number = Convert.ToInt32(num);
+    if (number > 0) count++;
+    volume--;
+}
+
+System.Console.WriteLine($"Количество чисел больше 0: {count}");
