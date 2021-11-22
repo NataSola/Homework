28b9e04e5ab4e380d4cc27202fe3fb4395e99666
NataSola <nsol2009@yandex.ru>
2021-11-22 13:14:19 +0300

    Add fib[0], fib[1]

--- 45_Fibonacci/Program.cs
+++ 45_Fibonacci/Program.cs
@@ -3,7 +3,7 @@
 Console.Clear();
 System.Console.WriteLine();
 
-int N = new Random().Next(1, 50);
+int N = new Random().Next(1, 40);
 System.Console.WriteLine($"Количество чисел Фибоначчи: {N}");
 
 int[] GetFibonacci(int N)
@@ -11,7 +11,9 @@ int[] GetFibonacci(int N)
     int prev1 = 1;
     int prev2 = 1;
     int[] fib = new int[N];
-    for (int i = 0; i < N; i++)
+    fib[0] = 1;
+    fib[1] = 1;
+    for (int i = 2; i < N; i++)
     {
         fib[i] = prev1 + prev2;
         prev2 = prev1;
@@ -22,7 +24,6 @@ int[] GetFibonacci(int N)
 
 void PrintArray(int[] arr)
 {
-    System.Console.Write("1 1 ");
     for (int i = 0; i < arr.Length; i++)
     {
         System.Console.Write(arr[i] + " ");
