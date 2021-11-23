5e3b3bddd015e11d50143fb1b7b881d5bcb99fa7
NataSola <nsol2009@yandex.ru>
2021-11-24 00:18:45 +0300

    Add find numder position

---
+++ 53_FindNumber/Program.cs
@@ -0,0 +1,81 @@
+﻿// 53. В двумерном массиве показать позиции числа, заданного пользователем или указать, что такого элемента нет
+
+Console.Clear();
+System.Console.WriteLine();
+
+int[,] CreateArray(int row, int col, int min, int max)
+{
+    int[,] array = new int[row, col];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(min, max);
+        }
+    }
+    return array;
+}
+
+int[] FindNumberPosition(int[,] arr, int number)
+{
+    int[] position = new int[2];
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            if (arr[i, j] == number)
+            {
+                position[0] = i;
+                position[1] = j;
+                return position;
+            }
+        }
+    }
+    position[0] = -1;
+    position[1] = -1;
+    return position;
+}
+
+void PrintArray(int[,] array)
+{
+    System.Console.WriteLine("Массив:");
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (array[i, j] / 1000 > 0) System.Console.Write($"{array[i, j]}   ");
+            else if (array[i, j] / 100 > 0) System.Console.Write($" {array[i, j]}   ");
+            else if (array[i, j] / 10 > 0) System.Console.Write($"  {array[i, j]}   ");
+            else System.Console.Write($"   {array[i, j]}   ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+void PrintPosition(int[] pos, int num)
+{
+    System.Console.WriteLine();
+    if (pos[0] >= 0 && pos[1] >= 0) System.Console.WriteLine($"Число {num} находится в {pos[0] + 1}-й строке, {pos[1] + 1}-м столбце");
+    else System.Console.WriteLine($"Число {num} отсутствует в заданном массиве");
+    System.Console.WriteLine();
+}
+
+int GetNumerToFind()
+{
+    System.Console.WriteLine();
+    System.Console.Write("Введите число:   ");
+    string writeNumber = Console.ReadLine();
+    int number = Convert.ToInt32(writeNumber);
+    return number;
+}
+
+int row = 7;
+int col = 10;
+int min = 1;
+int max = 1000;
+
+int[,] array1 = CreateArray(row, col, min, max);
+PrintArray(array1);
+int number = GetNumerToFind();
+int[] position = FindNumberPosition(array1, number);
+PrintPosition(position, number);
