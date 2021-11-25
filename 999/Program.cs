7894faede6fc6bc74fc35209116ab6c033810701
NataSola <nsol2009@yandex.ru>
2021-11-26 00:30:10 +0300

    add trick concate

---
+++ 999/Program.cs
@@ -0,0 +1,27 @@
+﻿// Объединить массивы
+
+int[] a = new int[] { 1, 2, 3 };
+int[] b = new int[] { 4, 5, 6 };
+int[] c = a.Concat(b).ToArray();
+System.Console.WriteLine(String.Join(' ', c));
+
+int[] createArray(int[] c)
+{
+    int[] cd = new int[6];
+    for (int i = 0; i < cd.Length; i++)
+    {
+        cd[i] = c[i] * 2;
+    }
+    return cd;
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        System.Console.Write(arr[i] + " ");
+    }
+}
+
+int[] d = createArray(c);
+PrintArray(d);
