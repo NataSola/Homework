f691562f2919ae41d489803ab7ed8ef4d693b60b
NataSola <nsol2009@yandex.ru>
2021-11-29 17:19:36 +0300

    copied tasks from the damaged folder

---
+++ 03_WeekDays/Program.cs
@@ -0,0 +1,14 @@
+﻿// 3. По введенному номеру дня недели вывести его название
+
+System.Console.WriteLine();
+string[] Days = { "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье" };
+
+System.Console.Write("Введите номер дня недели:   ");
+string writeDay = Console.ReadLine();
+bool result = int.TryParse(writeDay, out int dayNumber);
+
+System.Console.WriteLine();
+if (result == false) System.Console.WriteLine("Число введено неверно");
+else if (0 < dayNumber && dayNumber < 8) System.Console.WriteLine($"{dayNumber}-й день недели - {Days[dayNumber - 1]}");
+else System.Console.WriteLine("Дня недели с таким номером не существует");
+System.Console.WriteLine();
