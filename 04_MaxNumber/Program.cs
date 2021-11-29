190922bb3d8af32fffafcffa81ae095c71355bc1
NataSola <nsol2009@yandex.ru>
2021-11-29 17:32:00 +0300

    copied tasks from the damaged folder and fixed codes after checking by the teacher

---
+++ 04_MaxNumber/Program.cs
@@ -0,0 +1,18 @@
+﻿// 4. Найти максимальное из трех чисел 
+
+int[] numbers = { 145, 78, 68 };
+
+int GetMaxNumber(int[] array)
+{
+    int max = 0;
+    for (int i = 0; i < 3; i++)
+    {
+        if (numbers[i] > max) max = numbers[i];
+    }
+    return max;
+}
+
+System.Console.WriteLine();
+int maxNumber = GetMaxNumber(numbers);
+System.Console.WriteLine($"Максимальное число: {maxNumber}");
+System.Console.WriteLine();
