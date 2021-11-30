090244743c7484afe69c71b9549828b5798b3c36
NataSola <nsol2009@yandex.ru>
2021-11-30 14:35:42 +0300

    add comments

--- 999/Program.cs
+++ 999/Program.cs
@@ -15,7 +15,7 @@ int[] createArray(int[] c)
     return cd;
 }
 
-void PrintArray(int[] arr)
+void PrintArray(double[] arr)
 {
     for (int i = 0; i < arr.Length; i++)
     {
@@ -23,5 +23,46 @@ void PrintArray(int[] arr)
     }
 }
 
-int[] d = createArray(c);
-PrintArray(d);
+// int[] d = createArray(c);
+// PrintArray(d);
+
+//Масштабирование
+
+double k = 1.1;                                                          // коэффициент масштабирования
+double[] pointA = { 0, 0 };                                            // координаты точек
+double[] pointB = { 1, 2 };
+double[] pointC = { 4, 2 };
+double[] pointD = { 4, 0 };
+char[] pointName = { 'A', 'B', 'C', 'D' };
+
+double[] c1 = pointA.Concat(pointB).ToArray();
+double[] c2 = c1.Concat(pointC).ToArray();
+double[] c3 = c2.Concat(pointD).ToArray();
+
+double[] GerScalingCords(double[] arr, double k)
+{
+    double[] newCords = new double[arr.Length];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < 2) newCords[i] = arr[i];
+        else if (i > 1 && i % 2 == 0) newCords[i] = arr[i] * k - arr[0] * (k-1);
+        else if (i > 1 && i % 2 != 0) newCords[i] = arr[i] * k - arr[1] * (k-1);
+    }
+    return newCords;
+}
+
+
+
+
+System.Console.WriteLine();
+System.Console.WriteLine("---");
+System.Console.WriteLine(String.Join(' ', c3));
+
+System.Console.WriteLine("---");
+double[] newCords = GerScalingCords(c3, k);
+PrintArray(newCords);
+
+
+
+// "copied tasks from the damaged folder and fixed codes after checking by the teacher"
+
