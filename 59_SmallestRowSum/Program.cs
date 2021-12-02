bda811bfd624d5231fdb74e1f71fe5fefe4fee11
nsol2009@yandex.ru <nsol2009@yandex.ru>
2021-12-02 23:49:01 +0300

    Add the smallest amount in a row

---
+++ 59_SmallestRowSum/Program.cs
@@ -0,0 +1,60 @@
+﻿//  59. В прямоугольной матрице найти строку с наименьшей суммой элементов.
+
+int[,] GetMatrix(int row, int col, int min, int max)
+{
+    int[,] array = new int[row, col];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(min, max);
+        }
+    }
+    return array;
+}
+
+int rowNumber (int[,] array)
+{
+    int index = 0;
+    int sum = 0;
+    int result = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        sum = 0;
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            sum += array [i , j];
+        }
+           if (i == 0) result = sum;
+           else if (sum < result) 
+            {
+                result = sum;
+                index = i;
+            } 
+    }
+    return index;
+}
+
+void PrintMatrix(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write(array[i, j] + "  ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int row = 5;
+int col = 8;
+int min = 1;
+int max = 10;
+
+int[,] matrix = GetMatrix(row, col, min, max);
+PrintMatrix(matrix);
+System.Console.WriteLine("---");
+int index = rowNumber(matrix);
+System.Console.WriteLine(index);
+
