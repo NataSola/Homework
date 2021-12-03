79556174cec533bd80ef1be3d9787ff0c603e9d9
NataSola <nsol2009@yandex.ru>
2021-12-03 14:20:44 +0300

    Copied tasks from the damaged folder and fixed codes after checking by the teacher

---
+++ 17_SquareOrNo/Program.cs
@@ -0,0 +1,17 @@
+﻿// 17. По двум заданным числам проверять является ли одно квадратом другого
+
+Console.Clear();
+System.Console.WriteLine();
+
+int[] num = new int[2];
+string Result = "не является";
+
+for (int i = 0; i < 2; i++)
+{
+    num[i] = new Random().Next(1, 1000);
+}
+
+if (num[0] == num[1] * num[1]) Result = Result.Replace("не ", "");
+
+System.Console.WriteLine($"Число {num[0]} {Result} квадратом числа {num[1]}");
+System.Console.WriteLine();
