112943c434a050f4a6271590e03123e46d190ee7
nsol2009@yandex.ru <nsol2009@yandex.ru>
2021-12-05 02:06:21 +0300

    Add product of matrix

---
+++ 61_MultipleMatrix/Program.cs
@@ -0,0 +1,51 @@
+﻿// 61. Найти произведение двух матриц
+
+int[,] CreateMatrix(int row, int col, int min, int max)
+{
+    int[,] matrix = new int[row, col];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(min, max);
+        }
+    }
+    return matrix;
+}
+
+int[,] MultiplyMatrix(int[,] matrix1, int[,] matrix2)
+{
+    int[,] product = new int[matrix1.GetLength(0), matrix1.GetLength(1)];
+    for (int i = 0; i < matrix1.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix2.GetLength(1); j++)
+        {
+            product[i, j] = matrix1[i, j] * matrix2[i, j];
+        }
+    }
+    return product;
+}
+
+void PrintMatrix(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write(array[i, j] + " ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int row = 3;
+int col = 5;
+
+int[,] matrix1 = CreateMatrix(row, col, 1, 10);
+int[,] matrix2 = CreateMatrix(row, col, 10, 20);
+int[,] product = MultiplyMatrix(matrix1, matrix2);
+PrintMatrix(matrix1);
+System.Console.WriteLine("---");
+PrintMatrix(matrix2);
+System.Console.WriteLine("---");
+PrintMatrix(product);
