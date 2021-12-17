33d0853e15263b82060732d37ebb5d40437960f0
NataSola <nsol2009@yandex.ru>
2021-12-17 13:33:34 +0300

    Copied tasks from the damaged folder and fixed codes after checking

---
+++ 22_PointDistance/Program.cs
@@ -0,0 +1,50 @@
+﻿// 22. Найти расстояние между точками в пространстве 2D/3D
+
+Console.Clear();
+System.Console.WriteLine();
+
+int numPlan = 3;            // количество осей координат
+int min = -5;               // минимальное значение диапазона для выбора случайных чисел
+int max = 6;                // максимальное значение диапазона ...
+
+int[] GetPointCoords(int numPlan)
+{
+    int[] coords = new int[numPlan];
+    for (int i = 0; i < numPlan; i++)
+    {
+        coords[i] = new Random().Next(min, max);
+    }
+    return coords;
+}
+
+void PrintCoords(int[] array)
+{
+    System.Console.Write($"(");
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        System.Console.Write($"{array[i]}, ");
+    }
+    System.Console.WriteLine($"{array[array.Length - 1]})");
+}
+
+double FindDistance(int[] arr1, int[] arr2)
+{
+    int sum = 0;
+    for (int i = 0; i < arr1.Length; i++)
+    {
+        sum = sum + (arr1[i] - arr2[i]) * (arr1[i] - arr2[i]);
+    }
+    double result = Math.Sqrt(sum);
+    return result;
+}
+
+int[] PointA = GetPointCoords(numPlan);
+int[] PointB = GetPointCoords(numPlan);
+
+System.Console.Write($"Координаты точки А: ");
+PrintCoords(PointA);
+System.Console.Write($"Координаты точки B: ");
+PrintCoords(PointB);
+
+System.Console.WriteLine($"Расстояние между точками А и В: {FindDistance(PointA, PointB)}");
+System.Console.WriteLine();
---
+++ 26_Exponentiation/Program.cs
@@ -0,0 +1,22 @@
+﻿// 26. Возведите число А в натуральную степень B используя цикл
+Console.Clear();
+System.Console.WriteLine();
+
+int number = 2;
+int exponent = 10;
+
+int GetExponentiation(int number, int exponent)
+{
+    int count = 1;
+    int result = number;
+    while (count < exponent)
+    {
+        result = result * number;
+        count++;
+    }
+    return result;
+}
+
+int result = GetExponentiation(number, exponent);
+System.Console.WriteLine($"{number} ^ {exponent} = {result}");
+System.Console.WriteLine();
