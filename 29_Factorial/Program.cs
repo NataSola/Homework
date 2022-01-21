0b14a44f95dd029cf5cffc0241ce5f071a80be00
NataSola <nsol2009@yandex.ru>
2022-01-21 10:22:43 +0300

    Copied tasks from the damaged folder

---
+++ 29_Factorial/Program.cs
@@ -0,0 +1,21 @@
+﻿// 29. Написать программу вычисления произведения чисел от 1 до N
+
+Console.Clear();
+System.Console.WriteLine();
+
+System.Console.Write("Введите число:   ");
+string writeN = Console.ReadLine();
+int N = Convert.ToInt32(writeN);
+
+int GetFactorial(int N)
+{
+    int F = 1;
+    for (int num = 1; num <= N; num++)
+    {
+        F = F * num;
+    }
+    return F;
+}
+
+System.Console.WriteLine($"Произведение чисел от 1 до {N} равно {GetFactorial(N)}");
+System.Console.WriteLine();
